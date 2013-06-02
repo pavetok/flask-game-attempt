524a680badfe1257b5f995f849dc5a6e4e049cc5
pavetok <p.vetokhin@gmail.com>
2013-06-02 18:27:29 +0400

    Add many-to-many relationships.

--- app/models.py
+++ app/models.py
@@ -3,55 +3,107 @@ from hashlib import md5
 from app import db
 
 
-class Type(db.Model):
+object_category = db.Table('object_category',
+                       db.Column('obj_id', db.Integer,
+                                 db.ForeignKey('obj.id')),
+                       db.Column('category_id', db.Integer,
+                                 db.ForeignKey('category.id')))
+
+
+object_property = db.Table('object_property',
+                           db.Column('owner_id', db.Integer,
+                                     db.ForeignKey('obj.id')),
+                           db.Column('property_id', db.Integer,
+                                     db.ForeignKey('property.id')),
+                           db.Column('target_id', db.Integer,
+                                     db.ForeignKey('obj.id')))
+
+
+class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    timestamp = db.Column(db.DateTime)
 
     def __repr__(self):
-        return '<Type %r>' % (self.name)
+        return '<Category %r>' % (self.name)
 
 
-class Object(db.Model):
+class Obj(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    timestamp = db.Column(db.DateTime)
+    categories = db.relationship('Category',
+                              secondary=object_category,
+                              primaryjoin=(object_category.c.obj_id==id),
+                              secondaryjoin=(object_category.c.category_id==id),
+                              backref=db.backref('objects', lazy='dynamic'),
+                              lazy='dynamic',
+                              viewonly=True)
+    properties = db.relationship('Property',
+                              secondary = object_property,
+                              primaryjoin = (object_property.c.owner_id==id),
+                              secondaryjoin = (object_property.c.property_id==id),
+                              backref = db.backref('objects', lazy='dynamic'),
+                              lazy='dynamic',
+                              viewonly=True)
+
+    def add_category(self, category):
+        if not self.is_category(category):
+            self.categories.append(category)
+            return self
+
+    def del_category(self, category):
+        if not self.is_category(category):
+            self.categories.remove(category)
+            return self
+
+    def is_category(self, category):
+        return self.categories.filter(object_category.c.category_id == category.id).count() > 0
+
+    def add_property(self, prop):
+        if not self.is_property(prop):
+            self.properties.append(prop)
+            return self
+
+    def del_property(self, prop):
+        if not self.is_property(prop):
+            self.properties.remove(prop)
+            return self
+
+    def is_property(self, prop):
+        return self.properties.filter(object_property.c.property_id == prop.id).count() > 0
 
     def __repr__(self):
         return '<Object %r>' % (self.name)
 
 
-objects_types = db.Table('objects_types',
-                     db.Column('type', db.Integer,
-                               db.ForeignKey('type.id')),
-                     db.Column('object', db.Integer,
-                               db.ForeignKey('object.id')))
+active_properties = db.Table('relations',
+                    db.Column('active_property_id', db.Integer,
+                              db.ForeignKey('property.id')),
+                    db.Column('passive_property_id', db.Integer,
+                              db.ForeignKey('property.id')))
 
 
 class Property(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    timestamp = db.Column(db.DateTime)
+    passive_properties = db.relationship('Property',
+                               secondary=active_properties,
+                               primaryjoin=(active_properties.c.active_property_id==id),
+                               secondaryjoin=(active_properties.c.passive_property_id==id),
+                               backref=db.backref('active_properties', lazy='dynamic'),
+                               lazy='dynamic')
+
+    def add_passive(self, prop):
+        if not self.is_relation(prop):
+            self.passive_properties.append(prop)
+            return self
+
+    def del_relation(self, prop):
+        if self.is_relation(prop):
+            self.passive_properties.remove(prop)
+            return self
+
+    def is_relation(self, prop):
+        return self.passive_properties.filter(active_properties.c.passive_property_id == prop.id).count() > 0
 
     def __repr__(self):
         return '<Property %r>' % (self.name)
-
-
-objects_properties = db.Table('objects_properties',
-                         db.Column('owner', db.Integer,
-                                   db.ForeignKey('object.id')),
-                         db.Column('property', db.Integer,
-                                   db.ForeignKey('property.id')),
-                         db.Column('relates_to', db.Integer,
-                                   db.ForeignKey('object.id')))
-
-
-class Relationship(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True)
-    timestamp = db.Column(db.DateTime)
-    property_id = db.Column(db.Integer, db.ForeignKey('property.id'))
-    property_id = db.Column(db.Integer, db.ForeignKey('property.id'))
-
-    def __repr__(self):
-        return '<Relationship %r>' % (self.name)
---
+++ tests.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+#!flask/bin/python
+import unittest
+from datetime import datetime, timedelta
+from app import app, db, models
+from config import basedir
+from coverage import coverage
+import os
+
+cov = coverage(branch = True, omit = ['venv/*', 'tests.py'])
+cov.start()
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:395885@localhost/test.db'
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_object_category(self):
+        # create a category
+        c1 = models.Category(name='Heroes')
+        db.session.add(c1)
+        db.session.commit()
+        # create a obj
+        o1 = models.Obj(name='Figvan')
+        db.session.add(o1)
+        db.session.commit()
+        # add category
+        c1 = models.Category.query.get(1)
+        c11 = o1.add_category(c1)
+        db.session.add(c11)
+        db.session.commit()
+
+    def test_object_property(self):
+        # create properties
+        p1 = models.Property(name='Гладить')
+        db.session.add(p1)
+        db.session.commit()
+        # create a obj
+        o1 = models.Obj(name='Figvan')
+        db.session.add(o1)
+        db.session.commit()
+        # add categories
+        t1 = models.Category.query.get(1)
+        o11 = o1.add_property(p1)
+        db.session.add(o11)
+        db.session.commit()
+
+    def test_relationship(self):
+        # create properties
+        p1 = models.Property(name='Gladit')
+        p2 = models.Property(name='Gladitsya')
+        db.session.add(p1)
+        db.session.add(p2)
+        db.session.commit()
+        assert p1.del_relation(p2) == None
+        # create a relationship
+        p1 = models.Property.query.get(1)
+        p2 = models.Property.query.get(2)
+        p11 = p1.add_passive(p2)
+        db.session.add(p11)
+        db.session.commit()
+        assert p1.add_passive(p2) == None
+        print p1.passive_properties.first().name
+        assert p1.passive_properties.first().name == 'Gladitsya'
+        assert p2.active_properties.first().name == 'Gladit'
+
+if __name__ == '__main__':
+    try:
+        unittest.main()
+    except:
+        pass
+    cov.stop()
+    cov.save()
+    print "\n\nCoverage Report:\n"
+    cov.report()
+    print "HTML version: " + os.path.join(basedir, "tmp/coverage/index.html")
+    cov.html_report(directory = 'tmp/coverage')
+    cov.erase()
