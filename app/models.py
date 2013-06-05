1d0a86b51a5e46bacf3728112346b63bfd5057fb
pavetok <p.vetokhin@gmail.com>
2013-06-06 01:24:34 +0400

    Переделал схему БД. Теперь появились качества.

--- app/models.py
+++ app/models.py
@@ -3,17 +3,31 @@ from hashlib import md5
 from app import db
 
 
-object_category = db.Table('object_category',
-                           db.Column('obj_id', db.Integer,
-                                     db.ForeignKey('obj.id')),
-                           db.Column('category_id', db.Integer,
-                                     db.ForeignKey('category.id')))
+category_object = db.Table('category_object',
+                          db.Column('category_id', db.Integer,
+                                    db.ForeignKey('category.id')),
+                          db.Column('obj_id', db.Integer,
+                                    db.ForeignKey('obj.id')))
+
+
+category_property = db.Table('category_property',
+                             db.Column('category_id', db.Integer,
+                                       db.ForeignKey('category.id')),
+                             db.Column('property_id', db.Integer,
+                                       db.ForeignKey('property.id')))
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    properties = db.relationship('Category_Property', backref='category')
+    objects = db.relationship('Obj',
+                             secondary=category_object,
+                             backref=db.backref('categories', lazy='dynamic'),
+                             lazy='dynamic')
+    properties = db.relationship('Property',
+                                 secondary=category_property,
+                                 backref=db.backref('categories', lazy='dynamic'),
+                                 lazy='dynamic')
 
     def __repr__(self):
         return '<Category %r>' % self.name
@@ -22,11 +36,6 @@ class Category(db.Model):
 class Obj(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    categories = db.relationship('Category',
-                  secondary=object_category, primaryjoin=(object_category.c.obj_id==id),
-                  secondaryjoin=(object_category.c.category_id==id),
-                  backref=db.backref('objects', lazy='dynamic'),
-                  lazy='dynamic', viewonly=True)
 
     def add_category(self, category):
         if not self.is_category(category):
@@ -39,7 +48,7 @@ class Obj(db.Model):
             return self
 
     def is_category(self, category):
-        return self.categories.filter(object_category.c.category_id == category.id).count() > 0
+        return self.categories.filter(category_object.c.category_id==category.id).count() > 0
 
     def __repr__(self):
         return '<Object %r>' % self.name
@@ -53,35 +62,31 @@ class Property(db.Model):
         return '<Property %r>' % self.name
 
 
-class Category_Property(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), primary_key=True)
-    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), primary_key=True)
-    property = db.relationship("Property", backref='categories')
-
-
-class Operation(db.Model):
+class Relaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
-    active_property_id = db.Column(db.Integer, db.ForeignKey('property.id'), primary_key=True)
-    passive_property_id = db.Column(db.Integer, db.ForeignKey('property.id'), primary_key=True)
-    passive_property = db.relationship('Property',
-                                       primaryjoin="Property.id==Operation.passive_property_id")
-    active_property = db.relationship('Property',
-                                      primaryjoin="Property.id==Operation.active_property_id")
+    active_quality_id = db.Column(db.Integer, db.ForeignKey('quality.id'), unique=True)
+    passive_quality_id = db.Column(db.Integer, db.ForeignKey('quality.id'), unique=True)
+    active_quality = db.relationship('Quality',
+                                      primaryjoin="Quality.id==Relaction.active_quality_id")
+    passive_quality = db.relationship('Quality',
+                                      primaryjoin="Quality.id==Relaction.passive_quality_id")
+
+
+class Quality(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True)
+    is_active = db.Column(db.Boolean)
 
     def __repr__(self):
-        return '<Operation %r>' % self.name
+        return '<Quality %r>' % self.name
 
 
 class Knowledge(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    subject_id = db.Column(db.Integer, db.ForeignKey('obj.id'), primary_key=True)
-    object_id = db.Column(db.Integer, db.ForeignKey('obj.id'), primary_key=True)
-    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), primary_key=True)
+    subject_id = db.Column(db.Integer, db.ForeignKey('obj.id'))
+    obj_id = db.Column(db.Integer, db.ForeignKey('obj.id'))
     subject = db.relationship('Obj',
                               primaryjoin="Obj.id==Knowledge.subject_id")
-    object = db.relationship('Obj',
-                             primaryjoin="Obj.id==Knowledge.object_id")
-    property = db.relationship('Property',
-                               primaryjoin="Property.id==Knowledge.property_id")
+    obj = db.relationship('Obj',
+                          primaryjoin="Obj.id==Knowledge.obj_id")
--- tests.py
+++ tests.py
@@ -23,86 +23,118 @@ class TestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_object_category(self):
+    def test_category_object(self):
         # create a category
         c1 = models.Category(name='Heroes')
+        c2 = models.Category(name='Animals')
         db.session.add(c1)
+        db.session.add(c2)
         db.session.commit()
-        # create a obj
-        o1 = models.Obj(name='Figvan')
+        # create a object
+        o1 = models.Obj(name='Locky')
+        o2 = models.Obj(name='Cat')
         db.session.add(o1)
+        db.session.add(o2)
         db.session.commit()
-        # add category
+        # query instances
         c1 = models.Category.query.get(1)
-        c11 = o1.add_category(c1)
-        db.session.add(c11)
+        c2 = models.Category.query.get(2)
+        o1 = models.Obj.query.get(1)
+        o2 = models.Obj.query.get(2)
+        # append
+        o1.categories.append(c1)
+        c2.objects.append(o2)
+        db.session.add(o1)
+        db.session.add(c2)
         db.session.commit()
+        # query instances
+        c1 = models.Category.query.get(1)
+        c2 = models.Category.query.get(2)
+        o1 = models.Obj.query.get(1)
+        o2 = models.Obj.query.get(2)
         # assertions
-        print o1.categories
-        assert o1.categories.count() == 1
-        assert o1.categories.count() == 'Heroes'
-        assert c1.properties.first().name == 'Figvan'
+        assert o1.categories.first().name == 'Heroes'
+        assert c1.objects.first().name == 'Locky'
+        assert o2.categories.first().name == 'Animals'
+        assert c2.objects.first().name == 'Cat'
 
     def test_category_property(self):
         # create a category
         c1 = models.Category(name='Heroes')
+        c2 = models.Category(name='Animals')
         db.session.add(c1)
+        db.session.add(c2)
         db.session.commit()
-        # create an association instance
-        cp1 = models.Category_Property()
-        # create property
+        # create a property
         p1 = models.Property(name='Speak')
-        db.session.add(p1)
-        db.session.commit()
-        # add property to association instance
-        cp1.property = p1
-        # append association instance to category
-        c1.properties.append(cp1)
-        db.session.add(c1)
-        db.session.commit()
-        # assertions
-        assert c1.properties[0].property.name == 'Speak'
-        assert p1.categories[0].category.name == 'Heroes'
-
-    def test_operation(self):
-        # create properties
-        p1 = models.Property(name='Speak')
-        p2 = models.Property(name='Read')
+        p2 = models.Property(name='Shhhh')
         db.session.add(p1)
         db.session.add(p2)
         db.session.commit()
-        # create a relation instance
-        op1 = models.Operation(name='Speak-Read')
-        # add properties to relation
-        op1.active_property = p1
-        op1.passive_property = p2
-        db.session.add(op1)
+        # query instances
+        c1 = models.Category.query.get(1)
+        c2 = models.Category.query.get(2)
+        p1 = models.Property.query.get(1)
+        p2 = models.Property.query.get(2)
+        # append
+        p1.categories.append(c1)
+        c2.properties.append(p2)
+        db.session.add(p1)
+        db.session.add(c2)
         db.session.commit()
+        # query instances
+        c1 = models.Category.query.get(1)
+        c2 = models.Category.query.get(2)
+        p1 = models.Property.query.get(1)
+        p2 = models.Property.query.get(2)
         # assertions
-        assert op1.active_property.name == 'Speak'
-        assert op1.passive_property.name == 'Read'
+        assert p1.categories.first().name == 'Heroes'
+        assert c1.properties.first().name == 'Speak'
+        assert p2.categories.first().name == 'Animals'
+        assert c2.properties.first().name == 'Shhhh'
+
+    def test_relaction(self):
+        # create qualities
+        q1 = models.Quality(name='Consuming', is_active=True)
+        q2 = models.Quality(name='Eaten', is_active=False)
+        db.session.add(q1)
+        db.session.add(q2)
+        db.session.commit()
+        # create a relaction and add qualities
+        r1 = models.Relaction(name='Eat', active_quality=q1, passive_quality=q2)
+        # r1.active_quality = q1
+        # r1.passive_quality = q2
+        db.session.add(r1)
+        db.session.commit()
+        # query instance
+        r1 = models.Relaction.query.get(1)
+        # assertions
+        assert r1.name == 'Eat'
+        assert r1.active_quality.name == 'Consuming'
+        assert r1.passive_quality.name == 'Eaten'
 
     def test_knowledge(self):
         # create instances
         o1 = models.Obj(name='Figvan')
         o2 = models.Obj(name='Food')
-        p1 = models.Property(name='Edible')
         db.session.add(o1)
         db.session.add(o2)
-        db.session.add(p1)
         db.session.commit()
         # create a knowledge instance
         kn1 = models.Knowledge()
+        # query instances
+        o1 = models.Obj.query.get(1)
+        o2 = models.Obj.query.get(2)
         # add properties to relation
         kn1.subject = o1
-        kn1.object = o2
-        kn1.property = p1
+        kn1.obj = o2
         db.session.add(kn1)
         db.session.commit()
+        # query instance
+        kn1 = models.Knowledge.query.get(1)
         # assertions
         assert kn1.subject.name == 'Figvan'
-        assert kn1.object.name == 'Food'
-        assert kn1.property.name == 'Edible'
+        assert kn1.obj.name == 'Food'
 
 if __name__ == '__main__':
     try:
